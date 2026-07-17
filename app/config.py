b46c358b8242d203b8f10e245e2fc5a6236410c0
freyda-hash <mboumbafreyda87@gmail.com>
2026-07-17 03:58:38 +0200

    fix(project): arrange structure

--- app/config.py
+++ app/config.py
@@ -1,11 +1,9 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv(".env")
 
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
-DB_PASSWORD = os.getenv("DB_PASS")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_USER = os.getenv("DB_USER")
 APP_VERSION = os.getenv("APP_VERSION")
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from stack-apps!")
-
-
-if __name__ == "__main__":
-    main()
